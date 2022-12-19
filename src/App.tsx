249820085d9c7b580032a0164713402e0d0b65cd
danangsptro <danangsaputro300@gmail.com>
2022-12-19 16:18:23 +0700

    update todolist

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 
 function App() {
@@ -16,12 +17,13 @@ function App() {
     }
   };
 
-  console.log(todos )
+  console.log(todos);
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
